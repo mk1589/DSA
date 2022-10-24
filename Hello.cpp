6faee71ecf1eff7376584904359021aa1214f7d8
Manish Kumar Nirmalkar <82923667+mk1589@users.noreply.github.com>
2022-10-24 23:42:10 +0530

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,21 @@
+/*C++ is a widely used Object Oriented Programming language and is fairly easy to understand. Learning  C++ programming can be simplified into:  
+
+Writing your program in a text editor and saving it with correct extension(.CPP, .C, .CP)
+Compiling your program using a compiler or online IDE
+Understanding the basic terminologies.
+The “Hello World” program is the first step towards learning any programming language and is also one of the simplest programs you will learn.
+  All you have to do is display the message “Hello World” on the screen. Let us now look at the program: */
+
+
+/ Header file for input output functions
+#include <iostream>
+using namespace std;
+  
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World";
+  
+    return 0;
+}
